d30bab1d72e6fc7a19ecc8ba24e674f6fd26da64
Roben Kleene <contact@robenkleene.com>
2019-01-28 15:46:05 -0500

    Update constants

--- test/lib/test_constants.rb
+++ test/lib/test_constants.rb
@@ -3,12 +3,12 @@ TEST_LIB_DIRECTORY = File.dirname(__FILE__)
 # Template
 TEST_TEMPLATE_FILE = File.join(TEST_LIB_DIRECTORY, "../view/view.html.erb")
 
-# Base URL
-TEST_BASE_URL_PATH = File.join(TEST_LIB_DIRECTORY, "..")
-TEST_BASE_URL = "file://" + TEST_BASE_URL_PATH
+require "uri"
+TEST_ROOT_ACCESS_PATH = File.expand_path(File.join(TEST_LIB_DIRECTORY, ".."))
+TEST_ROOT_ACCESS_URL = URI.encode("file://" + TEST_ROOT_ACCESS_PATH)
 
 # Data Plugin
-TEST_DATA_DIRECTORY = File.join(TEST_LIB_DIRECTORY, "..", "data")
+TEST_DATA_DIRECTORY = File.join(TEST_LIB_DIRECTORY, "../data")
 DATA_PLUGIN_FILE = File.join(TEST_DATA_DIRECTORY, "Data.wcplugin")
 DATA_PLUGIN_NAME = "Data"
 DATA_PLUGIN_PATH_KEY = "Path"

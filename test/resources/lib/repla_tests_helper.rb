2ac854b4f6468b641ef85d94be45cd279ac32ce3
Roben Kleene <contact@robenkleene.com>
2019-01-30 12:30:56 -0500

    Fix some linter errors

--- test/resources/lib/repla_tests_helper.rb
+++ test/resources/lib/repla_tests_helper.rb
@@ -3,63 +3,71 @@ require 'Shellwords'
 module Repla
   module Tests
     module Helper
-      APPLESCRIPT_DIRECTORY = File.join(File.dirname(__FILE__), "..", "applescript")
+      APPLESCRIPT_DIRECTORY = File.join(File.dirname(__FILE__), '..',
+                                        'applescript')
 
       def self.run_javascript(javascript)
         # `osascript` requires redirecting stderr to stdout for some reason
-        return `osascript -l JavaScript -e #{Shellwords.escape(javascript)} 2>&1`.chomp!
+        `osascript -l JavaScript -e \
+        #{Shellwords.escape(javascript)} 2>&1`.chomp!
         # return `node -e #{Shellwords.escape(javascript)}`
       end
 
-      CONFIRMDIALOGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "confirm_dialog.applescript")
+      CONFIRMDIALOGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                                'confirm_dialog.applescript')
       def self.confirm_dialog
-        self.run_applescript(CONFIRMDIALOGAPPLESCRIPT_FILE)
+        run_applescript(CONFIRMDIALOGAPPLESCRIPT_FILE)
         sleep TEST_PAUSE_TIME # Give dialog time
       end
 
-      WINDOWIDAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "window_id.applescript")
+      WINDOWIDAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                           'window_id.applescript')
       def self.window_id
-        return self.run_applescript(WINDOWIDAPPLESCRIPT_FILE)
+        run_applescript(WINDOWIDAPPLESCRIPT_FILE)
       end
 
-      CANCELDIALOGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "cancel_dialog.applescript")
+      CANCELDIALOGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                               'cancel_dialog.applescript')
       def self.cancel_dialog
-        self.run_applescript(CANCELDIALOGAPPLESCRIPT_FILE)
+        run_applescript(CANCELDIALOGAPPLESCRIPT_FILE)
         sleep TEST_PAUSE_TIME # Give dialog time
       end
 
-      QUITAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "quit.applescript")
+      QUITAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                       'quit.applescript')
       def self.quit
-        self.run_applescript(QUITAPPLESCRIPT_FILE)
+        run_applescript(QUITAPPLESCRIPT_FILE)
       end
 
-      ISRUNNINGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "is_running.applescript")
+      ISRUNNINGAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                            'is_running.applescript')
       def self.is_running
-        result = self.run_applescript(ISRUNNINGAPPLESCRIPT_FILE)
-        if result == "true"
+        result = run_applescript(ISRUNNINGAPPLESCRIPT_FILE)
+        if result == 'true'
           return true
         else
           return false
         end
       end
 
-      SWITCHWINDOWSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "switch_windows.applescript")
+      SWITCHWINDOWSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                                'switch_windows.applescript')
       def self.switch_windows
-        self.run_applescript(SWITCHWINDOWSAPPLESCRIPT_FILE)
+        run_applescript(SWITCHWINDOWSAPPLESCRIPT_FILE)
       end
 
-      WINDOWBOUNDSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "window_bounds.applescript")
+      WINDOWBOUNDSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                               'window_bounds.applescript')
       def self.window_bounds(window_id = nil)
-        return self.run_applescript(WINDOWBOUNDSAPPLESCRIPT_FILE, [window_id])
+        run_applescript(WINDOWBOUNDSAPPLESCRIPT_FILE, [window_id])
       end
 
-      SETWINDOWBOUNDSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY, "set_window_bounds.applescript")
+      SETWINDOWBOUNDSAPPLESCRIPT_FILE = File.join(APPLESCRIPT_DIRECTORY,
+                                                  'set_window_bounds.applescript')
       def self.set_window_bounds(bounds, window_id = nil)
         arguments = [bounds]
-        if window_id
-          arguments = arguments.push(window_id)
-        end
-        self.run_applescript(SETWINDOWBOUNDSAPPLESCRIPT_FILE, arguments)
+        arguments = arguments.push(window_id) if window_id
+        run_applescript(SETWINDOWBOUNDSAPPLESCRIPT_FILE, arguments)
       end
 
       private
@@ -67,55 +75,50 @@ module Repla
       def self.run_applescript(script, arguments = nil)
         command = "osascript #{Shellwords.escape(script)}"
         if arguments
-          arguments.each { |argument|
+          arguments.each do |argument|
             if argument
               argument = argument.to_s
-              command = command + " " + Shellwords.escape(argument)
+              command = command + ' ' + Shellwords.escape(argument)
             end
-          }
+          end
         end
 
         result = `#{command}`
 
         result.chomp!
 
-        if result.empty?
-          return nil
-        end
+        return nil if result.empty?
 
-        if result.is_integer?
-          return result.to_i
-        end
+        return result.to_i if result.is_integer?
 
-        if result.is_float?
-          return result.to_f
-        end
+        return result.to_f if result.is_float?
 
-        return result
+        result
       end
 
       class ::String
         def is_float?
-          !!Float(self) rescue false
+          !!Float(self)
+        rescue StandardError
+          false
         end
 
         def is_integer?
-          self.to_i.to_s == self
+          to_i.to_s == self
         end
       end
 
       class ::Float
         def javascript_argument
-          return self.to_s
+          to_s
         end
       end
 
       class ::Integer
         def javascript_argument
-          return self.to_s
+          to_s
         end
       end
-
     end
   end
 end

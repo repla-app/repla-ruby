04e6e106c49c8bd51e129138973c60b8470fbe34
Roben Kleene <contact@robenkleene.com>
2019-01-31 06:32:52 -0500

    Run autocorrect

--- test/data/Data.wcplugin/Contents/Resources/lib/view.rb
+++ test/data/Data.wcplugin/Contents/Resources/lib/view.rb
@@ -1,6 +1,6 @@
 module Repla::Data
   class View < Repla::View
-    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), "../html")
+    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), '../html')
     VIEW_TEMPLATE = File.join(VIEWS_DIRECTORY, 'index.html')
 
     def initialize
@@ -9,16 +9,16 @@ module Repla::Data
       load_file(VIEW_TEMPLATE)
     end
 
-    ADD_KEY_VALUE_JAVASCRIPT_FUNCTION = "addKeyValue"
+    ADD_KEY_VALUE_JAVASCRIPT_FUNCTION = 'addKeyValue'.freeze
     def add_key_value(key, value)
       do_javascript_function(ADD_KEY_VALUE_JAVASCRIPT_FUNCTION, [key, value])
     end
-    
-    VALUE_FOR_KEY_JAVASCRIPT_FUNCTION = "valueForKey"
+
+    VALUE_FOR_KEY_JAVASCRIPT_FUNCTION = 'valueForKey'.freeze
     def value_for_key(key)
       value = do_javascript_function(VALUE_FOR_KEY_JAVASCRIPT_FUNCTION, [key])
       value.chomp!
-      return value
+      value
     end
   end
 end

36da33b83540dfa28c2d1facdd2aaad7d14cc94b
Roben Kleene <contact@robenkleene.com>
2019-02-01 15:18:49 -0500

    Run autocorrect

--- test/resources/bundles/Print.wcplugin/Contents/Resources/lib/view.rb
+++ test/resources/bundles/Print.wcplugin/Contents/Resources/lib/view.rb
@@ -1,7 +1,7 @@
 module Repla::Print
   class View < Repla::View
-    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), "..")
-    HTML_DIRECTORY = File.join(ROOT_ACCESS_DIRECTORY, "html")
+    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), '..')
+    HTML_DIRECTORY = File.join(ROOT_ACCESS_DIRECTORY, 'html')
     VIEW_TEMPLATE = File.join(HTML_DIRECTORY, 'index.html')
 
     def initialize
@@ -9,6 +9,5 @@ module Repla::Print
       self.root_access_directory_path = File.expand_path(BASE_DIRECTORY)
       load_file(VIEW_TEMPLATE)
     end
-
   end
 end

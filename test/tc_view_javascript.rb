80516935528bde4a1763f8ec712206c3746fd822
Roben Kleene <contact@robenkleene.com>
2019-02-05 14:27:07 -0500

    Swap parameters

--- test/tc_view_javascript.rb
+++ test/tc_view_javascript.rb
@@ -31,22 +31,22 @@ class TestViewJavaScript < Test::Unit::TestCase
 
   def test_javascript_function_without_arguments
     result = @view.do_javascript_function(TEST_JAVASCRIPT_FUNCTION_WITHOUT_ARGUMENTS_NAME)
-    assert_equal(result, TEST_JAVASCRIPT_FUNCTION_WITHOUT_ARGUMENTS_RESULT, "The result should equal the expected result.")
+    assert_equal(TEST_JAVASCRIPT_FUNCTION_WITHOUT_ARGUMENTS_RESULT, result, "The result should equal the expected result.")
   end
 
   def test_javascript_function_with_arguments
     result = @view.do_javascript_function(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_NAME, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_STRING_ARGUMENTS)
-    assert_equal(result, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_STRING_RESULT, "The result should equal the expected result.")
+    assert_equal(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_STRING_RESULT, result, "The result should equal the expected result.")
   end
 
   def test_javascript_function_with_integer_argument
     result = @view.do_javascript_function(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_NAME, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_INTEGER_ARGUMENTS)
-    assert_equal(result, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_INTEGER_RESULT, "The result should equal the expected result.")
+    assert_equal(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_INTEGER_RESULT, result, "The result should equal the expected result.")
   end
 
   def test_javascript_function_with_float_argument
     result = @view.do_javascript_function(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_NAME, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_FLOAT_ARGUMENTS)
-    assert_equal(result, TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_FLOAT_RESULT, "The result should equal the expected result.")
+    assert_equal(TEST_JAVASCRIPT_FUNCTION_WITH_ARGUMENTS_FLOAT_RESULT, result, "The result should equal the expected result.")
   end
 
 end

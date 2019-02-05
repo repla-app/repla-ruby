4fe081a57c6394e9a4ba5c665ffdeef877dc9c32
Roben Kleene <contact@robenkleene.com>
2019-02-05 14:22:04 -0500

    Fix path

--- test/lib/test_setup.rb
+++ test/lib/test_setup.rb
@@ -7,7 +7,7 @@ TEST_LIB_DIRECTORY = File.dirname(__FILE__)
 # Template
 TEST_TEMPLATE_FILE = File.join(TEST_LIB_DIRECTORY, '../html/index.html')
 
-TEST_ROOT_ACCESS_PATH = File.expand_path(File.join(TEST_LIB_DIRECTORY, '..'))
+TEST_ROOT_ACCESS_PATH = File.expand_path(File.join(TEST_LIB_DIRECTORY, '../../'))
 
 # Data Plugin
 TEST_DATA_DIRECTORY = File.join(TEST_LIB_DIRECTORY, '../data')

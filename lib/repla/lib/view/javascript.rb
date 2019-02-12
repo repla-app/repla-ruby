131717ade1303d0be74ca0e99fb6924e103236a5
Roben Kleene <contact@robenkleene.com>
2019-02-12 15:38:45 -0500

    Redo javascript argument

--- lib/repla/lib/view/javascript.rb
+++ lib/repla/lib/view/javascript.rb
@@ -1,4 +1,34 @@
+module JavaScriptArgument
+  module IntegerJavaScriptArgument
+    def javascript_argument
+      to_s
+    end
+  end
+
+  module StringJavaScriptArgument
+    def javascript_argument
+      "'#{javascript_escape}'"
+    end
+
+    def javascript_escape
+      gsub('\\', '\\\\\\\\').gsub("\n", '\\\\n').gsub("'", "\\\\'")
+    end
+
+    def javascript_escape!
+      replace(javascript_escape)
+    end
+  end
+
+  refine String do
+    include StringJavaScriptArgument
+  end
+
+  refine Integer do
+    include IntegerJavaScriptArgument
+  end
+end
 module Repla
+  using JavaScriptArgument
   class View < Window
     def do_javascript_function(function, arguments = nil)
       javascript = self.class.javascript_function(function, arguments)
@@ -22,28 +52,6 @@ module Repla
       end
 
       function << ');'
-    end
-
-    private
-
-    class ::Fixnum
-      def javascript_argument
-        to_s
-      end
-    end
-
-    class ::String
-      def javascript_argument
-        "'#{javascript_escape}'"
-      end
-
-      def javascript_escape
-        gsub('\\', '\\\\\\\\').gsub("\n", '\\\\n').gsub("'", "\\\\'")
-      end
-
-      def javascript_escape!
-        replace(javascript_escape)
-      end
     end
   end
 end

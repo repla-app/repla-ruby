dae887d9863db35c8dcc3694190bb7140841fed2
Roben Kleene <contact@robenkleene.com>
2019-02-13 14:11:42 -0500

    Fix some linter errors

--- lib/repla/test/bundles/TestEnvironment.wcplugin/Contents/Resources/test_environment.rb
+++ lib/repla/test/bundles/TestEnvironment.wcplugin/Contents/Resources/test_environment.rb
@@ -6,24 +6,25 @@ require_relative '../../../../../../lib/repla'
 require_relative 'constants'
 require_relative '../../../../../lib/escape'
 
+# Test environment
 class TestEnviroment < Test::Unit::TestCase
   using Escape
   def test_plugin_name_key
-    assert(ENV.key?(Repla::PLUGIN_NAME_KEY), 'The plugin name key should exist.')
+    assert(ENV.key?(Repla::PLUGIN_NAME_KEY))
     plugin_name = ENV[Repla::PLUGIN_NAME_KEY]
-    assert_equal(plugin_name, TEST_PLUGIN_NAME, 'The plugin name should equal the test plugin name.')
+    assert_equal(plugin_name, TEST_PLUGIN_NAME)
   end
 
   def test_split_id_key
-    assert(ENV.key?(Repla::SPLIT_ID_KEY), 'The split id key should exist.')
+    assert(ENV.key?(Repla::SPLIT_ID_KEY))
     window_id = ENV[Repla::SPLIT_ID_KEY]
-    assert(!window_id.integer?, 'The split id should not be an integer.')
+    assert(!window_id.integer?)
   end
 
   def test_window_id_key
-    assert(ENV.key?(Repla::WINDOW_ID_KEY), 'The window id key should exist.')
+    assert(ENV.key?(Repla::WINDOW_ID_KEY))
     window_id = ENV[Repla::WINDOW_ID_KEY]
-    assert(window_id.integer?, 'The window id should be an integer.')
-    assert(window_id.to_i > 0, 'The window id should be greater than zero.')
+    assert(window_id.integer?)
+    assert(window_id.to_i > 0)
   end
 end

568b7cefedaf2e478ba8a1bd2c4ca862710bb41b
Roben Kleene <contact@robenkleene.com>
2019-02-21 12:34:20 -0500

    Add second test

--- test/tc_escape.rb
+++ test/tc_escape.rb
@@ -7,10 +7,18 @@ require_relative '../lib/repla/lib/escape.rb'
 # Test escape
 class TestEscape < Test::Unit::TestCase
   using Escape
-  def test_shell_escape
+  def test_shell_escape_path
     string = '/../../applescript/exists.scpt'
     result = string.shell_escape
     test_result = '/../../applescript/exists.scpt'
+    assert_equal(test_result, result)
+  end
+
+  def test_shell_escape_string
+    string = 'This is a test string'
+    result = string.shell_escape
+    test_result = 'result = This\ is\ a\ test\ string\'
+\''
     assert_equal(test_result, result)
   end
 end

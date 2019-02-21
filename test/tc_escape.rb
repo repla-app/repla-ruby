1c9841df0b4c46b3cb1705affc06109fa84e9d27
Roben Kleene <contact@robenkleene.com>
2019-02-21 12:31:26 -0500

    Add first assert

--- test/tc_escape.rb
+++ test/tc_escape.rb
@@ -4,7 +4,13 @@ require 'test/unit'
 
 require_relative '../lib/repla/lib/escape.rb'
 
+# Test escape
 class TestEscape < Test::Unit::TestCase
   using Escape
+  def test_shell_escape
+    string = '/../../applescript/exists.scpt'
+    result = string.shell_escape
+    test_result = '/../../applescript/exists.scpt'
+    assert_equal(test_result, result)
+  end
 end
-

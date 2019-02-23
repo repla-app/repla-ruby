56f5e0fede51f28601c68b8197a5e13754f67635
Roben Kleene <contact@robenkleene.com>
2019-02-23 15:04:58 -0500

    Flesh out view helper

--- lib/repla/test/lib/view_helper.rb
+++ lib/repla/test/lib/view_helper.rb
@@ -1,10 +1,24 @@
+require_relative '../../test'
+
 module Repla
   module Test
     # View helper
     module ViewHelper
       def make_views(filename)
+        file_view = make_file_view(filename)
+        url_view = make_url_view(filename)
+        [file_view, url_view]
       end
-      def make_view(filename)
+
+      def make_file_view(filename)
+        file = html_file(filename)
+        view = Repla::Window.new
+        view.load_file(file)
+        view
+      end
+
+      def make_url_view(filename)
+        file = html_server_url(filename)
         view = Repla::Window.new
         view.load_file(file)
         view
@@ -12,4 +26,3 @@ module Repla
     end
   end
 end
-

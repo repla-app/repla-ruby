4040f0ffd2b727ad68cc2d25310c436559082218
Roben Kleene <contact@robenkleene.com>
2019-02-23 17:18:14 -0500

    Add make windows command

--- lib/repla/test/lib/view_helper.rb
+++ lib/repla/test/lib/view_helper.rb
@@ -4,22 +4,28 @@ module Repla
   module Test
     # View helper
     module ViewHelper
+      def self.make_windows(filename)
+        file_view = make_file_view(filename, Window.new)
+        url_view = make_url_view(filename, Window.new)
+        [file_view, url_view]
+      end
+
       def self.make_views(filename)
         file_view = make_file_view(filename)
         url_view = make_url_view(filename)
         [file_view, url_view]
       end
 
-      def self.make_file_view(filename)
+      def self.make_file_view(filename, initializer = View.new)
         file = Repla::Test.html_file(filename)
-        view = Repla::View.new
+        view = initializer
         view.load_file(file)
         view
       end
 
-      def self.make_url_view(filename)
+      def self.make_url_view(filename, initializer = View.new)
         url = Repla::Test.html_server_url(filename)
-        view = Repla::View.new
+        view = initializer
         view.load_url(url)
         view
       end

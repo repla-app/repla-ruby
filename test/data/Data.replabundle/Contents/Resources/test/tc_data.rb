72daa6b06d4046668a6c02e35311ea6929cac95c
Roben Kleene <contact@robenkleene.com>
2019-03-18 12:34:25 -0400

    Migrate another test

--- test/data/Data.replabundle/Contents/Resources/test/tc_data.rb
+++ test/data/Data.replabundle/Contents/Resources/test/tc_data.rb
@@ -10,18 +10,18 @@ class TestClass < Minitest::Test
     path = File.expand_path(TEST_DATA_DIRECTORY)
 
     Repla.load_plugin(DATA_PLUGIN_FILE)
-    Repla.run_plugin(DATA_PLUGIN_NAME, path, arguments.split(' '))
-
-    window_id = Repla.window_id_for_plugin(DATA_PLUGIN_NAME)
+    window_id = Repla.run_plugin(DATA_PLUGIN_NAME, path, arguments.split(' '))
     window = Repla::Window.new(window_id)
 
-    sleep Repla::Test::TEST_PAUSE_TIME # Give time for script to run
-
     javascript = %[valueForKey('#{DATA_PLUGIN_PATH_KEY}');]
-    path_result = window.do_javascript(javascript)
+    path_result = nil
+    Repla::Test.block_until do
+      path_result = window.do_javascript(javascript)
+      path_result == path
+    end
+
     javascript = %[valueForKey('#{DATA_PLUGIN_ARGUMENTS_KEY}');]
     arguments_result = window.do_javascript(javascript)
-
     assert_equal(path, path_result)
     assert_equal(arguments, arguments_result)
     window.close

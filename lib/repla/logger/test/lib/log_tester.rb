2ef4069f796e63ae7526135286ca793b6033e513
Roben Kleene <contact@robenkleene.com>
2019-05-14 14:51:02 -0400

    Make test more reliable

--- lib/repla/logger/test/lib/log_tester.rb
+++ lib/repla/logger/test/lib/log_tester.rb
@@ -37,34 +37,53 @@ module Repla
         STDERR.puts "Error messages #{error_messages}"
         return false
       end
-      expected = MESSAGES.count
-      result = messages.count
+      # Buffering issues are causing the output of `TestLog.replaplugin` to be
+      # unreliable, so for now we're running it through `/usr/bin/script`.
+      # Which is reliable, but standard out and standard error get combined.
+      combined_expected = ERROR_MESSAGES + MESSAGES
+      combined_result = error_messages + messages
+      expected = combined_expected.count
+      result = combined_result.count
       if expected != result
         STDERR.puts "Expected #{expected} messages instead of #{result}"
         return false
       end
-      expected = ERROR_MESSAGES.count
-      result = error_messages.count
-      if ERROR_MESSAGES.count != error_messages.count
-        STDERR.puts "Expected #{expected} error messages instead of #{result}"
-        return false
-      end
-      (0..MESSAGES.count).each do |i|
-        expected = MESSAGES[i]
+      (0..combined_expected.count).each do |i|
+        expected = combined_expected[i]
         result = messages[i]
         if expected != result
           STDERR.puts "At index #{i}, expected message #{expected} instead of #{result}"
           return false
         end
       end
-      (0..ERROR_MESSAGES.count).each do |i|
-        expected = ERROR_MESSAGES[i]
-        result = error_messages[i]
-        if expected != result
-          STDERR.puts "At index #{i}, expected error message #{expected} instead of #{result}"
-          return false
-        end
-      end
+      # expected = MESSAGES.count
+      # result = messages.count
+      # if expected != result
+      #   STDERR.puts "Expected #{expected} messages instead of #{result}"
+      #   return false
+      # end
+      # expected = ERROR_MESSAGES.count
+      # result = error_messages.count
+      # if ERROR_MESSAGES.count != error_messages.count
+      #   STDERR.puts "Expected #{expected} error messages instead of #{result}"
+      #   return false
+      # end
+      # (0..MESSAGES.count).each do |i|
+      #   expected = MESSAGES[i]
+      #   result = messages[i]
+      #   if expected != result
+      #     STDERR.puts "At index #{i}, expected message #{expected} instead of #{result}"
+      #     return false
+      #   end
+      # end
+      # (0..ERROR_MESSAGES.count).each do |i|
+      #   expected = ERROR_MESSAGES[i]
+      #   result = error_messages[i]
+      #   if expected != result
+      #     STDERR.puts "At index #{i}, expected error message #{expected} instead of #{result}"
+      #     return false
+      #   end
+      # end
       true
     end
   end
--- lib/repla/test/packages/TestLog.replaplugin/Contents/Resources/echo.sh
+++ lib/repla/test/packages/TestLog.replaplugin/Contents/Resources/echo.sh
@@ -4,8 +4,8 @@ set -e
 
 echo 'Testing log error' >&2
 printf "\n" >&2
-echo 'Testing log message'
 echo 'ERROR ' >&2
+echo 'Testing log message'
 echo 'MESSAGE '
 echo
 printf "\t"

2fdfc84cdf260d3c39973bcaa78f1c4c6d378aec
Roben Kleene <contact@robenkleene.com>
2019-10-28 20:31:20 -0400

    Add a clean path method

--- lib/repla/lib/constants.rb
+++ lib/repla/lib/constants.rb
@@ -4,6 +4,7 @@ module Repla
   SPLIT_ID_KEY = 'REPLA_SPLIT_ID'.freeze
   WINDOW_ID_KEY = 'REPLA_WINDOW_ID'.freeze
   DARK_MODE_KEY = 'REPLA_DARK_MODE'.freeze
+  PATH_PREFIX = 'REPLA_PATH_PREFIX'.freeze
 
   # Directories
   APPLESCRIPT_DIRECTORY = File.join(__dir__, '../../applescript')

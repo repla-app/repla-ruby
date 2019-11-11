5186672e5b97d17fff50348a2b4a025f1a0cc58e
Roben Kleene <contact@robenkleene.com>
2019-11-11 18:08:30 -0500

    Improve portability

--- lib/repla/test/packages/Echo.replaplugin/Contents/Resources/echo.sh
+++ lib/repla/test/packages/Echo.replaplugin/Contents/Resources/echo.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [[ -n "$1" ]]; then
   echo "$@"
--- lib/repla/test/packages/TestLog.replaplugin/Contents/Resources/run.sh
+++ lib/repla/test/packages/TestLog.replaplugin/Contents/Resources/run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e
-script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+script_dir="$( cd "$(/usr/bin/dirname "$0")" ; pwd -P )"
 echo_path="$script_dir/echo.sh"
 /usr/bin/script -q /dev/null "$echo_path"
